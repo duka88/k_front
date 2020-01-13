d4f7577d093558efcf32d959e3d368f67624b865
duka88 <djankov88@gmail.com>
2020-01-13 10:48:02 +0100

    'asd'

---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <div v-for="recipe in recipes.data">
+       <nuxt-link :to="`recipes/${recipe.slug}`">{{recipe.title}}</nuxt-link>
+    </div>
+  </div>
+</template>
+<script>
+import gql from 'graphql-tag';
+
+export default {
+  components: {
+
+  },
+  apollo: {
+        recipes: gql `
+        query{
+          recipes(first: 2){
+            data{
+              title
+              slug
+          
+        }
+      }
+    }`
+  }
+}
+
+</script>
+
---
+++ pages/recipes/_slug/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <h1>{{$route.params.slug}}</h1>
+</template>
+<script>
+import gql from 'graphql-tag';
+
+export default {
+  components: {
+
+  },
+  apollo: {
+    recipe: {
+      query: gql `
+        query getRecipe($slug: slug){
+          recipes(slug: $slug){
+            data{
+              title
+          
+        }
+      }
+    }`,
+      variables() {
+        return {
+          slug: this.$route.params.slug
+        }
+      }
+    }
+  }
+}
+
+</script>
